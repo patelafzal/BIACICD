0feda557c6ae4197cd4faa28f7fca6f1c8e3f38d
Afzal Patel <afzal.patel09@outlook.com>
2025-06-14 09:26:35 +0530

    CI test 2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 // frontend/src/index.js
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// CI test 1
+// CI test 2
 import App from './App';
 
 const container = document.getElementById('root');
